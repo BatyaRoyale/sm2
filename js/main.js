e1c77fd97a8ed2bac27531737f99fdd1f5f1a91c
BatyaRoyale <saih.niyaz@gmail.com>
2023-09-07 15:27:52 +0300

    sm2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let city = prompt("Название города?");
+let yearcity = prompt("Год образования города");
+let people = prompt("Население");
+
+let date = new Date();
+age = (date - yearcity);
+alert(age);
+
+alert ("Городу "+ city +" исполнилось "+ age +" лет с момента его образования. Население -" + people) ;
